4418c3c11bd282999cf6d5b8cfd6116b60029b30
Josh Freeman <jdfreeman@satx.rr.com>
2013-11-08 01:15:07 -0600

    Adjusting Config Definitions

--- apps/config/loader.php
+++ apps/config/loader.php
@@ -6,11 +6,11 @@ $loader = new \Phalcon\Loader();
 
 $loader->registerNamespaces(
     array(
-        'ProfitPress\Components' => '/var/www/profitpress/apps/components',//__APPSDIR__.'components',
-        'ProfitPress\Offers' => '/var/www/profitpress/apps/offers',//__APPSDIR__.'components',
-        'ProfitPress\Blog' => '/var/www/profitpress/apps/blog',//__APPSDIR__.'components',
-        'ProfitPress\Site' => '/var/www/profitpress/apps/site',//__APPSDIR__.'components',
-        'ProfitPress\Permalink' => '/var/www/profitpress/apps/permalink',//__APPSDIR__.'components',
+        'ProfitPress\Components' => __APPSDIR__.'components',
+        'ProfitPress\Offers' => __APPSDIR__.'offers',
+        'ProfitPress\Blog' => __APPSDIR__.'blog',
+        'ProfitPress\Site' => __APPSDIR__.'site',
+        'ProfitPress\Permalink' => __APPSDIR__.'permalink',
     )
 );
 
